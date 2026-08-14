8369dbfd1fe97a6ad25ff5dfc378e37d0c831cbb
Wilson5104 <emmanglenwongyan@gmail.com>
2026-08-14 19:13:27 +0800

    error fix

---
+++ lab2/page3.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Wong Yan's Biography";
+$header = "Currently";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+        <body>
+            <h1><?php echo $header; ?></h1>
+            <p>After graduating from St. Ana De Victorias, Im currently pursuing my studies at University of St. La Salle</p>
+            -<br>
+            <p>As a Com Sci Student and want to persue a career in Game Development. Although we don't know what the future holds.</p><br><p>I'm still on my 3rd year so I'm still growing as a person and will keep growing.</p>
+            <br>
+            <img src="USLS.png" alt="USLS">
+            <br><br>
+            <a href="index.php">Home</a> | 
+            <a href="page2.php">High School</a>
+            </body>
+</html>
