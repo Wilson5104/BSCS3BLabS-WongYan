5cdbb0d243353762ec76b7e48c77c56ca73038fa
Wilson5104 <emmanglenwongyan@gmail.com>
2026-08-14 18:49:56 +0800

    Lab2 Complete

---
+++ lab2/index.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Wong Yan's Biography";
+$header = "Home";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+        <body>
+            <h1><?php echo $header; ?></h1>
+            <p>This is the introductory page of my biography. I am Emman Glen Wong Yan and this is part of my life.</p>
+            <br>
+            <h2>Elementary</h2>
+            <p>when I was a kid I went to "Jack and Jill" in Victorias near the Lady of Victory parish church.</p>
+            <br>
+            <img src="Jack_and_Jill.png" alt="Jack_and_Jill">
+            <br><br>
+            <a href="page2.php">High School</a> | 
+            <a href="page3.php">Currently</a>
+            </body>
+</html>
---
+++ lab2/page2.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Wong Yan's Biography";
+$header = "High School";
+?>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+        <body>
+            <h1><?php echo $header; ?></h1>
+            <p>After graduating from elementary school, at first I want to go to Don Bosco yet I went to Colegio St. Ana De Victorias.</p>
+            -<br>
+            <p>Though at first I was hesitant, I eventually adapted to the new environment. And had many great memories there.</p>
+            <br>
+            <img src="St_Anna.png" alt="St_Anna">
+            <br><br>
+            <a href="index.php">Home</a> | 
+            <a href="page3.php">Contact</a>
+            </body>
+</html>
